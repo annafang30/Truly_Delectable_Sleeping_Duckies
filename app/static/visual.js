4b510028b021b349bb60858f349ca294ad33451d
Anna Fang <afang30@stuy.edu>
2023-05-05 13:19:28 -0400

    hovering

--- app/static/visual.js
+++ app/static/visual.js
@@ -1,22 +1,32 @@
 var c = document.getElementById("can");
 var map = document.getElementById("map");
 var ctx = c.getContext("2d"); 
+var spacing =  65;
+// map.src = "Happiness Score.png";
+width = ctx.canvas.width = window.innerWidth; 
+height = ctx.canvas.height = window.innerHeight; 
 
-  // map.src = "Happiness Score.png";
+let Kentucky = new Path2D(); 
+Kentucky.arc(width/2, height/2, 30,0,2*Math.PI); 
+ctx.stroke(Kentucky);
+ctx.fillStyle = "red"; 
+ctx.fill(States); 
 
+c.addEventListener("mousemove", (event) => {
+  const isPointInPath = ctx.isPointInPath(Kentucky, event.offsetX, event.offsetY);
+  if(isPointInPath) {
+    ctx.fillStyle = "green";
+  }
+  else {
+    ctx.fillStyle = "red";
+  }
+  ctx.fill(Kentucky);
+});
 
-var drawMap = (e) => {
-    ctx.canvas.width = window.innerWidth; 
-    ctx.canvas.height = window.innerHeight; 
-    
-    drawState(ctx.canvas.width/2, ctx.canvas.height/2);
-}
 
 function drawState(x,y) {
   ctx.beginPath(); 
-  ctx.arc(x, y, 10, 0, 2*Math.PI);
-  ctx.fillStyle = "green";
+  ctx.arc(x, y, 30, 0, 2*Math.PI);
+  ctx.fillStyle = "red";
   ctx.fill(); 
 }
-
-map.addEventListener("click", drawMap);
