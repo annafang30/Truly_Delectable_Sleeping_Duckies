dbb00039e4c80d7b2a317b744a68b1608736b3da
Samson Wu <swu30@stuy.edu>
2023-05-06 12:59:52 -0400

    states array

--- app/static/visual.js
+++ app/static/visual.js
@@ -1,32 +1,44 @@
 var c = document.getElementById("can");
 var map = document.getElementById("map");
 var ctx = c.getContext("2d"); 
+
 var spacing =  65;
 // map.src = "Happiness Score.png";
-width = ctx.canvas.width = window.innerWidth; 
-height = ctx.canvas.height = window.innerHeight; 
+const width = ctx.canvas.width = window.innerWidth; 
+const height = ctx.canvas.height = window.innerHeight; 
+const NUM_STATES = 50;
+const STATE_NAMES = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 
-let Kentucky = new Path2D(); 
-Kentucky.arc(width/2, height/2, 30,0,2*Math.PI); 
-ctx.stroke(Kentucky);
-ctx.fillStyle = "red"; 
-ctx.fill(States); 
+function draw_state(x, y){
+  let RADIUS = 30;
+  let DEFAULT_COLOR = "red";
+
+  let path = new Path2D(); 
+  ctx.fillStyle = DEFAULT_COLOR;
+  path.arc(x, y, RADIUS, 0, 2 * Math.PI); 
+  ctx.stroke(path);
+  ctx.fill(path);
+  
+  return path;
+}
+
+const states = [];
+
+// populate 50 states
+for(let i = 0; i < NUM_STATES; i++){
+  states[i] = draw_state(50 + 65*(i%10), 50 + 65 * (Math.floor(i/10)));
+}
 
 c.addEventListener("mousemove", (event) => {
-  const isPointInPath = ctx.isPointInPath(Kentucky, event.offsetX, event.offsetY);
-  if(isPointInPath) {
-    ctx.fillStyle = "green";
+  for(let i = 0; i < NUM_STATES; i++){    
+    const isPointInPath = ctx.isPointInPath(states[i], event.offsetX, event.offsetY);
+    if(isPointInPath) {
+      ctx.fillStyle = "green";
+      console.log(STATE_NAMES[i]);
+    }
+    else {
+      ctx.fillStyle = "red";
+    }
+    ctx.fill(states[i]);
   }
-  else {
-    ctx.fillStyle = "red";
-  }
-  ctx.fill(Kentucky);
 });
-
-
-function drawState(x,y) {
-  ctx.beginPath(); 
-  ctx.arc(x, y, 30, 0, 2*Math.PI);
-  ctx.fillStyle = "red";
-  ctx.fill(); 
-}
