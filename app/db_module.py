8400bf031e85727a8fa1528ccb4a3249d573def3
Samson Wu <swu30@stuy.edu>
2023-05-12 10:42:56 -0400

    moved happiness and wages off repo and into db

--- app/db_module.py
+++ app/db_module.py
@@ -1,6 +1,33 @@
 import sqlite3
 from datetime import datetime
+import json
+import requests
+
 DB_FILE="database.db"
+MCBROKEN_URL = "https://raw.githubusercontent.com/rashiq/mcbroken-archive/main/mcbroken.json"
+STATE_HAPPINESS_URL = "https://raw.githubusercontent.com/samsonahh/state_stats/main/happy_states.json"
+STATE_WAGE_URL = "https://raw.githubusercontent.com/samsonahh/state_stats/main/minimum_wages.json"
+
+try:
+    mcbroken_json = requests.get(MCBROKEN_URL)
+    MCBROKEN = [i for i in json.loads(
+        mcbroken_json.text) if i["properties"]["country"] == "USA"]
+except:
+    MCBROKEN = {}
+
+try:
+    happy_json = requests.get(STATE_HAPPINESS_URL)
+    HAPPINESS = json.loads(happy_json.text)
+except:
+    HAPPINESS = {}
+
+try:
+    wage_json = requests.get(STATE_WAGE_URL)
+    MINIMUM_WAGES = json.loads(wage_json.text)
+except:
+    MINIMUM_WAGES = {}
+
+STATES = { 'AK': 'Alaska',  'AL': 'Alabama', 'AR': 'Arkansas', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado',  'CT': 'Connecticut', 'DE': 'Delaware','FL': 'Florida',  'GA': 'Georgia', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas','KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MS': 'Mississippi', 'MT': 'Montana', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
 
 def reset_database():
     db = sqlite3.connect(DB_FILE)
@@ -13,8 +40,41 @@ def reset_database():
 
     c.execute("CREATE TABLE IF NOT EXISTS Users(username TEXT, password TEXT, userID INTEGER);")
     c.execute("CREATE TABLE IF NOT EXISTS Forum(postID INTEGER, parentID INTEGER, username TEXT, text TEXT, date TEXT);")
-
+    c.execute("CREATE TABLE IF NOT EXISTS States(state TEXT, state_short TEXT, happiness REAL, wage REAL);")
+    
     db.commit()
+    db.close()
+
+def add_state_data():
+    if len(HAPPINESS) == 0 or len(MINIMUM_WAGES) == 0:
+        return
+
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    h = []
+    w = []
+
+    for happy in HAPPINESS:
+        h.append(happy)
+    
+    for wage in MINIMUM_WAGES:
+        w.append(wage)
+
+    for i in range(len(h)):
+        c.execute("INSERT INTO States VALUES (?, ?, ?, ?)", 
+                 (h[i]["state"], list(STATES.keys())[list(STATES.values()).index(h[i]["state"])], h[i]["totalScore"], w[i]["minimumWage"]))
+    
+    db.commit()
+    db.close()  
+
+def print_all_states():
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db          
+    c = db.cursor() #creates db cursor to execute and fetch      
+
+    c.execute("SELECT * FROM States")
+    dict = c.fetchall()
+    print(dict)
     db.close()
 
 def add_newuser(username, password):
@@ -83,7 +143,6 @@ def get_all_users():
     db.close()
 
     return [i[0] for i in dict]
-    
 
 
 def print_all_users():
@@ -141,8 +200,66 @@ def print_all_forum_posts():
     print(dict)
     db.close()
 
+def get_ratio_broken_by_state(state):
+    broken = len([i for i in MCBROKEN if i["properties"]["is_broken"]
+                 and i["properties"]["is_active"] and i["properties"]["state"] == state])
+    total = len([i for i in MCBROKEN if i["properties"]["state"] == state])
+    if total == 0:
+        return None
+    return broken/(total)
+
+def get_state_stats(state):
+    state_name = ""
+    happiness = 0
+    wage = 0
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db          
+    c = db.cursor() #creates db cursor to execute and fetch      
+
+    c.execute("SELECT * FROM States")
+    dict = c.fetchall()
+    db.close()
+
+    for s in dict:
+        if s[1] == state:
+            state_name = s[0]
+            happiness = s[2]
+            wage = s[3]
+    try:
+        return {
+            "name": state_name,
+            "min_wage": "$" + make_money_two_decimals(wage),
+            "happiness": happiness,
+            "broken_ratio": str(round(get_ratio_broken_by_state(state)*100, 2)) + "%"
+        }
+    except:
+        return {
+            "name": state_name,
+            "min_wage": "$" + make_money_two_decimals(wage),
+            "happiness": happiness,
+            "broken_ratio": "N/A"
+        }
+
+def get_all_states_stats():
+    all = {}
+    for key in list(STATES.keys()):
+        all[key] = get_state_stats(key)
+    return all
+
+def make_money_two_decimals(float):
+    if str(float).find('.') == -1:
+        return str(float) + ".00"
+    
+    after_zero = str(float).split('.')[1]
+    if len(after_zero) == 1:
+        return str(float) + "0"
+    return str(float)
+
 def generate_preset_database():
     reset_database()
+    
+    add_state_data()
+
     add_newuser('samson', 'samson123')
     add_newuser('anna', 'anna123')
     add_newuser('ravindra', 'ravindra123')
