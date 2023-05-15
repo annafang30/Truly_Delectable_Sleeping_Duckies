e9d480be5723e84cbd31c9a22d30067633b103b0
Anna Fang <afang30@stuy.edu>
2023-05-15 16:26:17 -0400

    add two line graphs

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -1,12 +1,13 @@
 // import Chart from 'chart.js/auto';
 var c = document.getElementById("myChart");
 var d = document.getElementById("myChart2"); 
-// example data to get this working ^^
+var e = document.getElementById("myChart3"); 
 STATES = { 'AK': 'Alaska',  'AL': 'Alabama', 'AR': 'Arkansas', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado',  'CT': 'Connecticut', 'DE': 'Delaware','FL': 'Florida',  'GA': 'Georgia', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas','KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MS': 'Mississippi', 'MT': 'Montana', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
 const keys = Object.keys(STATES);  
 console.log(keys); 
 const happiness = []; 
 const brokenness = []; 
+var coordinates = []; 
 
 for(let i = 0; i<50; i++){
   happiness[i] = stats[keys[i]]["happiness"]
@@ -18,7 +19,7 @@ for(let r = 0; r<50; r++){
 console.log(happiness);
 console.log(brokenness);
   
-
+// single axes
 const data = {
   labels: keys,
   datasets: [
@@ -26,7 +27,7 @@ const data = {
     label: "Happiness",
     data: happiness,
     fill: false,
-    borderColor: 'rgb(75, 192, 192)',
+    borderColor: 'blue',
     tension: 0.1
   },
   {
@@ -38,7 +39,6 @@ const data = {
   }
 ]
 }
-// might have to change to a scatterplot to make the correlation factor more apparent? 
 
 new Chart(c, {
     type: "line",
@@ -46,21 +46,69 @@ new Chart(c, {
     options: {
       responsive: true,
       maintainAspectRatio: false, 
-    //   ticks: {
-    //     stepSize: 0.5
-    // }
+      title:{
+        display: true, 
+        text: "Happiness vs. Brokenness :("
       }
+      },
 
 }); 
 
-// new Chart(c, {
-//   type: "scatter",
-//   data: data,
-//   options: {
-//     responsive: true, 
-//     maintainAspectRatio:false,
-//   }
-// });
+// dual axes
+const data2 = {
+  labels: keys,
+  datasets: [
+  {
+    label: "Happiness",
+    data: happiness,
+    fill: false,
+    borderColor: 'blue',
+    tension: 0.1,
+    yAxisID: 'y'
+
+  },
+  {
+    label: 'Brokenness', 
+    // yAxisID: "right",
+    data: brokenness,
+    fill:false, 
+    borderColor: "red", 
+    tension: 0.1,
+    yAxisID: 'y2'
+  }
+]
+}
+
+new Chart(d, {
+  type: 'line',
+  data: data2,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [ // I literally tried to do this the normal way and IT DOESN'T WORK so this is my workaround TT 
+      {
+        id: 'y',
+        type: 'linear',
+        display: true,
+        position: 'left',
+      },
+      {
+        id: 'y2',
+        type: 'linear',
+        display: true,
+        position: 'right',
+      },
+    ]
+    },
+    title:{
+      display: true, 
+      text: "Happiness vs. Brokenness :("
+    },
+  },
+});
+
+// attempting scatterplot ...
 
 const scatterData = {
   datasets: [{
@@ -82,22 +130,12 @@ const scatterData = {
   }],
 }
 
-
-new Chart(d, {
+new Chart(e, {
   labels: keys,
   type: 'scatter',
   data: scatterData,
   options: {
-    scales: {
-      x: {
-        type: 'linear',
-        position: 'bottom'
-      }
-    }
+    
   }
 });
-
-
-
-
 
