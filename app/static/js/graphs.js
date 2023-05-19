69ad44b729795595124ad7d737584646ca407b34
Anna Fang <afang30@stuy.edu>
2023-05-19 10:26:05 -0400

    toggling graphs done

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -1,26 +1,15 @@
-// import Chart from 'chart.js/auto';
-//var c = document.getElementById("myChart");
-var d = document.getElementById("myChart2"); 
-d.style.display = "none"; 
-var e = document.getElementById("myChart3"); 
-var f = document.getElementById("myChart4"); 
-var g = document.getElementById("myChart5"); 
-
-var mixButton = document.getElementById("mixed"); 
-console.log(d.style.display);
+var c = document.getElementById("myChart");
+var ctx = c.getContext("2d"); 
 
 STATES = { 'AK': 'Alaska',  'AL': 'Alabama', 'AR': 'Arkansas', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado',  'CT': 'Connecticut', 'DE': 'Delaware','FL': 'Florida',  'GA': 'Georgia', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas','KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MS': 'Mississippi', 'MT': 'Montana', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
 const keys = Object.keys(STATES);  
-// console.log(keys); 
+
 const happiness = []; 
 const brokenness = []; 
 const wages = [];
 const coordinates = []; 
 const coordinates2 = []; 
 const coordinates3 = []; 
-
-console.log(wages); 
-console.log(coordinates2);
 
 for(let i = 0; i<50; i++){
   happiness[i] = stats[keys[i]]["happiness"]
@@ -45,84 +34,86 @@ for(let j = 0; j<50; j++){
 for(let b = 0; b<50; b++){
   coordinates3[b] = {x: brokenness[b], y: wages[b]}; 
 }
- console.log(coordinates);
-// console.log(brokenness);
   
 // single axes
-// const data = {
-//   labels: keys,
-//   datasets: [
-//   {
-//     label: "Happiness",
-//     data: happiness,
-//     fill: false,
-//     borderColor: 'blue',
-//     tension: 0.1
-//   },
-//   {
-//     label: 'Brokenness', 
-//     data: brokenness,
-//     fill:false, 
-//     borderColor: "red", 
-//     tension: 0.1 
-//   }
-// ]
-// }
 
-// new Chart(c, {
-//     type: "line",
-//     data: data,
-//     options: {
-//       responsive: true, 
-//       maintainAspectRatio: false, 
-//       title:{
-//         display: true, 
-//         text: "Happiness vs. Brokenness :("
-//       },
-//       scales:{
-//         yAxes:[{
-//           scaleLabel:{
-//             display: true, 
-//             labelString: "happiness OR brokenness",
-//           },
-//         },],
-//         xAxes:[{
-//           scaleLabel:{
-//             display: true, 
-//             labelString: "state",
-//           },
-//         },],
-//       },
-//       },
-//
-//}); 
-
-function mixedLine(){
-  // dual axes
-  const data2 = {
-    labels: keys,
-    datasets: [
-    {
-      label: "Happiness",
-      data: happiness,
-      fill: false,
-      borderColor: 'blue',
-      tension: 0.1,
-      yAxisID: 'y'
-    },
-    {
-      label: 'Brokenness', 
-      // yAxisID: "right",
-      data: brokenness,
-      fill:false, 
-      borderColor: "red", 
-      tension: 0.1,
-      yAxisID: 'y2'
-    }
-  ]
+const data = {
+  labels: keys,
+  datasets: [
+  {
+    label: "Happiness",
+    data: happiness,
+    fill: false,
+    borderColor: 'blue',
+    tension: 0.1
+  },
+  {
+    label: 'Brokenness', 
+    data: brokenness,
+    fill:false, 
+    borderColor: "red", 
+    tension: 0.1 
   }
+]
+}
 
-  new Chart(d, {
+var drawChart1 = function(e){
+  return new Chart(c, {
+    type: "line",
+    data: data,
+    options: {
+      responsive: true, 
+      maintainAspectRatio: false, 
+      title:{
+        display: true, 
+        text: "Happiness vs. Brokenness :("
+      },
+      scales:{
+        yAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "happiness OR brokenness",
+          },
+        },],
+        xAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "state",
+          },
+        },],
+      },
+      },
+})
+}
+
+drawChart1(); // => shows as the default graph 
+
+// dual axes
+const data2 = {
+  labels: keys,
+  datasets: [
+  {
+    label: "Happiness",
+    data: happiness,
+    fill: false,
+    borderColor: 'blue',
+    tension: 0.1,
+    yAxisID: 'y'
+  },
+  {
+    label: 'Brokenness', 
+    // yAxisID: "right",
+    data: brokenness,
+    fill:false, 
+    borderColor: "red", 
+    tension: 0.1,
+    yAxisID: 'y2'
+  }
+]
+}
+
+var drawChart2 = function(e){
+  return new Chart(c, {
     type: 'line',
     data: data2,
     options: {
@@ -166,8 +157,11 @@ function mixedLine(){
         text: "Happiness vs. Brokenness :("
       },
     },
-  });
+  })
 };
+
+
+// scatter of happiness v brokenness
 
 const scatterData = {
   datasets: [{
@@ -177,36 +171,37 @@ const scatterData = {
   }],
 }
 
-// scatter of happiness v brokenness
-new Chart(e, {
-  labels: keys,
-  type: 'scatter',
-  data: scatterData,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales:{
-      yAxes:[{
-        scaleLabel:{
-          display: true, 
-          labelString: "happiness",
-        },
-      },],
-      xAxes:[{
-        scaleLabel:{
-          display: true, 
-          labelString: "brokenness",
-        },
-      },],
+var drawChart3 = function(e){
+  return new Chart(c, {
+    labels: keys,
+    type: 'scatter',
+    data: scatterData,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales:{
+        yAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "happiness",
+          },
+        },],
+        xAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "brokenness",
+          },
+        },],
+      },
+      title:{
+        display: true, 
+        text: "Happiness vs. Brokenness :("
+      },
     },
-    title:{
-      display: true, 
-      text: "Happiness vs. Brokenness :("
-    },
-  },
-});
+  })
+}; 
 
-// scattergraph - min wage vs happiness 
+// // scattergraph - min wage vs happiness 
 
 const scatterData2 = {
   datasets: [{
@@ -216,35 +211,37 @@ const scatterData2 = {
   }],
 }
 
-new Chart(f, {
-  labels: keys,
-  type: 'scatter',
-  data: scatterData2,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales:{
-      yAxes:[{
-        scaleLabel:{
-          display: true, 
-          labelString: "happiness",
-        },
-      },],
-      xAxes:[{
-        scaleLabel:{
-          display: true, 
-          labelString: "wages",
-        },
-      },],
+var drawChart4 = function(e){
+  return new Chart(c, {
+    labels: keys,
+    type: 'scatter',
+    data: scatterData2,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales:{
+        yAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "happiness",
+          },
+        },],
+        xAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "wages",
+          },
+        },],
+      },
+      title:{
+        display: true, 
+        text: "Happiness vs. Wages :("
+      },
     },
-    title:{
-      display: true, 
-      text: "Happiness vs. Wages :("
-    },
-  },
-});
+  })
+};
 
-// wages as compared to rate of broken ice cream machines
+// scatter of wages versus brokenness 
 const scatterData3 = {
   datasets: [{
     label: '',
@@ -253,42 +250,78 @@ const scatterData3 = {
   }],
 }
 
-new Chart(g, {
-  labels: keys,
-  type: 'scatter',
-  data: scatterData3,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales:{
-      yAxes:[{
-        scaleLabel:{
-          display: true, 
-          labelString: "wages",
-        },
-      },],
-      xAxes:[{
-        scaleLabel:{
-          display: true, 
-          labelString: "brokenness",
-        },
-      },],
+var drawChart5 = function(e) {
+  return new Chart(c, {
+    labels: keys,
+    type: 'scatter',
+    data: scatterData3,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales:{
+        yAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "wages",
+          },
+        },],
+        xAxes:[{
+          scaleLabel:{
+            display: true, 
+            labelString: "brokenness",
+          },
+        },],
+      },
+      title:{
+        display: true, 
+        text: "Wages vs. Broken Ice Cream Machines :("
+      },
     },
-    title:{
-      display: true, 
-      text: "Wages vs. Broken Ice Cream Machines :("
-    },
-  },
-});
+  })  
+}; 
 
-function toggleDisplay(canv) {
-  if (canv.style.display == "none") {
-    canv.style.display = "block";
-  }
-  else {
-    canv.style.display = "none";
-  }
+// all event listeners 
+
+var singleAxis = () =>{
+  console.log("single"); 
+  ctx.clearRect(0, 0, c.width, c.height);
+  drawChart1(); 
 };
 
-mixButton.addEventListener("click", toggleDisplay(d));
-mixButton.addEventListener("click", mixedLine());
+var dualAxis = () => {
+  console.log("dual"); 
+  ctx.clearRect(0, 0, c.width, c.height);
+  drawChart2(); 
+};
+
+var drawscatter1 = () => {
+  console.log("scatter1");
+  ctx.clearRect(0, 0, c.width, c.height);
+  drawChart3(); 
+}
+
+var drawscatter2 = () => {
+  console.log("scatter2"); 
+  ctx.clearRect(0, 0, c.width, c.height);
+  drawChart4(); 
+}
+
+var drawscatter3 = () => {
+  console.log("scatter3"); 
+  ctx.clearRect(0, 0, c.width, c.height);
+  drawChart5(); 
+}
+
+
+var singleButton = document.getElementById("singleline"); 
+var dualButton = document.getElementById("dualline"); 
+var scatter1 = document.getElementById("scatter"); 
+var scatter2 = document.getElementById("scatter2"); 
+var scatter3 = document.getElementById("scatter3"); 
+
+
+singleButton.addEventListener("click", singleAxis); 
+dualButton.addEventListener("click", dualAxis); 
+scatter1.addEventListener("click", drawscatter1);  
+scatter2.addEventListener("click", drawscatter2); 
+scatter3.addEventListener("click", drawscatter3); 
